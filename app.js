76c28192caf83a58ce1d028adfca9c644a965b95
keenin99 <anakato4@gmail.com>
2021-08-29 21:45:35 +0300

    add changes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const hamburger=document.querySelector(".hamburger");
 const navLinks =document.querySelector(".nav-links" );
 const links=document.querySelectorAll(".nav-links li");
-hamburger.addEventListener('click', ()=>{
-navLinks.classList.toggle('open');
+hamburger.addEventListener("click", ()=>{
+navLinks.classList.toggle("open");
+links.forEach(link=>{
+links.classList.toggle("fade");
+});
 });
